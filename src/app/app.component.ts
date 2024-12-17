22207cc2e9a003da3b81af9aae207b212a8abf5f
will <113859999+Williamwsm@users.noreply.github.com>
2024-12-17 00:21:07 -0300

    logo sony

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+import { PlanoFundoComponent } from "./pages/plano-fundo/plano-fundo.component";
+import { MenuPrincipalComponent } from "./components/card/menu-principal/menu-principal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, MenuBarComponent],
+  imports: [RouterOutlet, HomeComponent, MenuBarComponent, PlanoFundoComponent, MenuPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/plano-fundo/plano-fundo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plano-fundo',
+  standalone: true,
+  imports: [],
+  templateUrl: './plano-fundo.component.html',
+  styleUrl: './plano-fundo.component.css'
+})
+export class PlanoFundoComponent {
+
+}
